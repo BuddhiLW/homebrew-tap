e5fc6b12277bad7d48b40b5dbf53e78990efab4a
clojurewasm-release <clojurewasm-release@users.noreply.github.com>
2026-09-01 03:17:30 +0000

    cljw v1.13.4

--- Formula/cljw.rb
+++ Formula/cljw.rb
@@ -5,15 +5,15 @@ class Cljw < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/BuddhiLW/ClojureWasm/releases/download/v1.13.2/cljw-macos-aarch64.tar.gz"
-      sha256 "1e8ed80928daaed620f85a2e4b2619b64ea5d24dabfb2508f56e17da8f6cf3d9"
+      url "https://github.com/BuddhiLW/ClojureWasm/releases/download/v1.13.4/cljw-macos-aarch64.tar.gz"
+      sha256 "44ba7690d5b993123585dca5ac9867a7a73aefa3f9c3052836740b10cacb5f8e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/BuddhiLW/ClojureWasm/releases/download/v1.13.2/cljw-linux-x86_64.tar.gz"
-      sha256 "33438d5df522bab06d78e9be04aa4a97a245724daafb8acdaddd47e0f56359b6"
+      url "https://github.com/BuddhiLW/ClojureWasm/releases/download/v1.13.4/cljw-linux-x86_64.tar.gz"
+      sha256 "d56a249da77ba1e345f53d352ddd85905258b6744fe2c373cb27179da5e1dd29"
     end
   end
 
@@ -23,6 +23,6 @@ class Cljw < Formula
 
   test do
     assert_equal "3", shell_output("#{bin}/cljw -e '(+ 1 2)'").strip
-    assert_match "1.13.2", shell_output("#{bin}/cljw --version")
+    assert_match "1.13.4", shell_output("#{bin}/cljw --version")
   end
 end
